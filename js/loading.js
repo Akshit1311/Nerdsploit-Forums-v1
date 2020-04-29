ba971d0bfe68462f15150628c08e04d03ccff80f
Akshit1311 <a4748g@gmail.com>
2020-04-29 16:14:44 +0530

    Preloader Updateddgit status

---
+++ js/loading.js
@@ -0,0 +1,17 @@
+ // $(document).ready(function(){
+        // 	$('div#loading').removeAttr('id');
+      // });
+      // var preloader = document.getElementById("loading"); <<<
+      // window.addEventListener('load', function(){
+      // 	preloader.style.display = 'none';
+      // 	})
+  
+      // function preLoader(){
+      //   preloader.style.display = 'none';                      <<<<<<<<<<
+      // };
+
+
+
+      $(document).ready(function(){
+        $("#loading").slideUp();
+      });
